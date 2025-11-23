52babdb048baffeead5cbdc420d3a05f1f3352f0
soumia <soudev2003@gmail.com>
2025-11-23 19:33:45 +0100

    update task_repository.dart

---
+++ lib/domain/repositories/task_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:to_do_list_app/domain/entities/task_entity.dart';
+
+abstract class TaskRepository {
+  Future<Either<String, List<TaskEntity>>> getAllTasks();
+  Future<Either<String, Unit>> addTask(TaskEntity taskEntity);
+  Future<Either<String, Unit>> updateTask(TaskEntity taskEntity);
+  Future<Either<String, Unit>> deleteTask(String id);
+}
