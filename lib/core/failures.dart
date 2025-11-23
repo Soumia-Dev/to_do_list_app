7476f9bb48ee9ba4e39623f4edc460e74eff8c95
soumia <soudev2003@gmail.com>
2025-11-23 19:20:22 +0100

    update failures.dart

--- lib/core/failures.dart
+++ lib/core/failures.dart
@@ -2,3 +2,19 @@ abstract class Failures {
   final String message;
   const Failures(this.message);
 }
+
+class CacheFailure extends Failures {
+  const CacheFailure(super.message);
+}
+
+class DatabaseFailure extends Failures {
+  const DatabaseFailure(super.message);
+}
+
+class NotFoundFailure extends Failures {
+  const NotFoundFailure(super.message);
+}
+
+class UnexpectedFailure extends Failures {
+  const UnexpectedFailure(super.message);
+}
