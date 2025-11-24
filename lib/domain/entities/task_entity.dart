62476aa1d765f3ff3fc6dbcb0a503a48c9ced478
soumia <soudev2003@gmail.com>
2025-11-24 16:40:03 +0100

    update task_entity.dart

--- lib/domain/entities/task_entity.dart
+++ lib/domain/entities/task_entity.dart
@@ -1,7 +1,7 @@
 class TaskEntity {
   final String? id;
   final String title;
-  final String subtitle;
+  final String description;
   final DateTime createdAt;
   final DateTime? updatedAt;
   final bool isDone;
@@ -9,7 +9,7 @@ class TaskEntity {
   const TaskEntity({
     this.id,
     required this.title,
-    required this.subtitle,
+    required this.description,
     required this.createdAt,
     this.updatedAt,
     required this.isDone,
