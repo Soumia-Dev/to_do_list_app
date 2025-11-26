75040f5a285f87da25ad5994571f75714b386488
soumia <soudev2003@gmail.com>
2025-11-26 13:07:39 +0100

    upload task_model.g.dart and update task_model.dart files

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -1,30 +1,50 @@
+import 'package:hive/hive.dart';
 import 'package:to_do_list_app/domain/entities/task_entity.dart';
 
-class TaskModel extends TaskEntity {
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
+class TaskModel extends HiveObject {
+  @HiveField(0)
+  final int? hiveId;
+  @HiveField(1)
+  final String hiveTitle;
+  @HiveField(2)
+  final String hiveDescription;
+  @HiveField(3)
+  final DateTime hiveCreatedAt;
+  @HiveField(4)
+  final DateTime hiveUpdatedAt;
+  @HiveField(5)
+  final bool hiveIsDone;
+
   TaskModel({
-    super.id,
-    required super.title,
-    required super.description,
-    required super.createdAt,
-    required super.isDone,
+    this.hiveId,
+    required this.hiveTitle,
+    required this.hiveDescription,
+    required this.hiveCreatedAt,
+    required this.hiveUpdatedAt,
+    required this.hiveIsDone,
   });
-  factory TaskModel.fromJson(Map<String, dynamic> json) {
+
+  factory TaskModel.fromEntity(TaskEntity entity) {
     return TaskModel(
-      id: json['id'],
-      title: json['title'],
-      description: json['description'],
-      createdAt: json['createdAt'],
-      isDone: json['isDone'],
+      hiveTitle: entity.title,
+      hiveDescription: entity.description,
+      hiveCreatedAt: entity.createdAt,
+      hiveUpdatedAt: entity.updatedAt,
+      hiveIsDone: entity.isDone,
     );
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'title': title,
-      'description': description,
-      'createdAt': createdAt,
-      'isDone': isDone,
-    };
+  TaskEntity toEntity() {
+    return TaskEntity(
+      id: hiveId,
+      title: hiveTitle,
+      description: hiveDescription,
+      createdAt: hiveCreatedAt,
+      updatedAt: hiveUpdatedAt,
+      isDone: hiveIsDone,
+    );
   }
 }
---
+++ lib/data/models/task_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskModelAdapter extends TypeAdapter<TaskModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TaskModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskModel(
+      hiveId: fields[0] as int?,
+      hiveTitle: fields[1] as String,
+      hiveDescription: fields[2] as String,
+      hiveCreatedAt: fields[3] as DateTime,
+      hiveUpdatedAt: fields[4] as DateTime,
+      hiveIsDone: fields[5] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.hiveId)
+      ..writeByte(1)
+      ..write(obj.hiveTitle)
+      ..writeByte(2)
+      ..write(obj.hiveDescription)
+      ..writeByte(3)
+      ..write(obj.hiveCreatedAt)
+      ..writeByte(4)
+      ..write(obj.hiveUpdatedAt)
+      ..writeByte(5)
+      ..write(obj.hiveIsDone);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
