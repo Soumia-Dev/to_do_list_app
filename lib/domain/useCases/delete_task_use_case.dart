c691be0e11e1d3ca522df30a44939f6ff89a33d4
soumia <soudev2003@gmail.com>
2025-11-29 21:23:09 +0100

    update dalete_task_use_case.dart

--- lib/domain/useCases/delete_task_use_case.dart
+++ lib/domain/useCases/delete_task_use_case.dart
@@ -5,7 +5,7 @@ import '../repositories/task_repository.dart';
 class DeleteTaskUseCase {
   final TaskRepository taskRepository;
   DeleteTaskUseCase(this.taskRepository);
-  Future<Either<String, Unit>> call(int id) async {
-    return await taskRepository.deleteTask(id);
+  Future<Either<String, Unit>> call(Set<int> ids) async {
+    return await taskRepository.deleteTask(ids);
   }
 }
